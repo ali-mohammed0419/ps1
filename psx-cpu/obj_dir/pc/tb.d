02f9d92565f6e1acce6cc74c8e22c42393b7f621
Ali Mohammed <ali.mohammed0419@gmail.com>
2026-08-12 13:30:05 -0500

    Ignore build artifcats

--- psx-cpu/obj_dir/pc/tb.d
+++
@@ -1,8 +0,0 @@
-tb.o: /Users/alimohammed/Projects/FPGA/Verilator/PS1/psx-cpu/sim/tb.cpp \
-  /Users/alimohammed/Projects/FPGA/Verilator/PS1/psx-cpu/sim/tb.h \
-  Vcpu32.h \
-  /opt/homebrew/Cellar/verilator/5.050/share/verilator/include/verilated.h \
-  /opt/homebrew/Cellar/verilator/5.050/share/verilator/include/verilated_config.h \
-  /opt/homebrew/Cellar/verilator/5.050/share/verilator/include/verilatedos.h \
-  /opt/homebrew/Cellar/verilator/5.050/share/verilator/include/verilated_types.h \
-  /opt/homebrew/Cellar/verilator/5.050/share/verilator/include/verilated_funcs.h
